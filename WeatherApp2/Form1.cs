c528553aae2601131dafd65eae76cc40b18df89c
Stephen Robbins <10696812@uvu.edu>
2020-04-18 10:15:12 -0600

    Committed because the App loads the data now, into the Combobox.

--- WeatherApp2/Form1.cs
+++ WeatherApp2/Form1.cs
@@ -16,30 +16,28 @@ namespace WeatherApp2
         public Form1()
         {
             InitializeComponent();
-            Cbox1Load()
+            Cbox1Load();
         }
         private void Cbox1Load()
         {
             //Set Select as the Cb first word to load.
             Cbox1.Items.Add("Select");
+            Cbox1.SelectedIndex = 0;
             //Set the filename string variable to an escape sequence.
-            string filename = @"C:\Users\Administrator\source\repos\WeatherApp2\WeatherApp2\bin\Debug\WorldCities.txt";
-            //We will open the file text using the filepath, or escape sequence. //also, assign this to readerVar to be called.
+            string filename = @"C:\Users\Administrator\source\repos\WeatherApp2\WeatherApp2\bin\Debug\WorldCities";
+            //Create a string-list Contents to be called later for the filename.
+            string[] Contents;
+            //This is where the majic happens. //We will open the file text using the filepath, or escape sequence. 
             StreamReader inputFile = File.OpenText(filename);
             //Use StreamReader to see if you're at the end of the file.
             while (!inputFile.EndOfStream)
             {
-
-                string[] stringList1;
-                
-                
-                stringList1 = filename.Split('\n');
+                //Add each city to a string-list called contents.
+                Contents = inputFile.ReadLine().Split('\n');
                 //Here, we chuck all the city names into a combobox for dropdown funcionality.
-                Cbox1.Items.Add(stringList1);
-
-                //break to get out of the loop.
-                break;
+                Cbox1.Items.Add(Contents);
             }
+            inputFile.Close();
         }
 
         private void Cbox1_SelectedIndexChanged(object sender, EventArgs e)
